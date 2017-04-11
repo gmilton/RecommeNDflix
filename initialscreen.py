15fe545ee3ee5d018f4dbca0bf100909d250ba92
Grace Milton <gmilton@nd.edu>
2017-04-11 16:26:50 -0400

    Updating title

--- initialscreen.py
+++ initialscreen.py
@@ -14,35 +14,37 @@ def main():
     background.fill((185, 9, 11))
 
     # Display title screen
+    text_vertical = 100;
+    text_horizontal = 97; 
     outline_font = pygame.font.Font(None, 52)
-    outline = outline_font.render("RecommeNDflix", 1, (10, 10, 10))
+    outline = outline_font.render("R E C O M M E N D F L I X", 1, (10, 10, 10))
     font = pygame.font.Font(None, 52)
-    text = font.render("RecommeNDflix", 1, (255, 255, 255))
-    background.blit(outline, (165,99)) # 1 up, 1 left
-    background.blit(outline, (166,99))
-    background.blit(outline, (167,99))
-    background.blit(outline, (168,99))
-    background.blit(outline, (169,99))
+    text = font.render("R E C O M M E N D F L I X", 1, (255, 255, 255))
+    background.blit(outline, (text_horizontal - 2,text_vertical - 1)) # 1 up, 1 left
+    background.blit(outline, (text_horizontal - 1,text_vertical - 1))
+    background.blit(outline, (text_horizontal,text_vertical - 1))
+    background.blit(outline, (text_horizontal + 1,text_vertical - 1))
+    background.blit(outline, (text_horizontal + 2,text_vertical - 1))
     #background.blit(outline, (170,99)) # 1 down, 1 left
     #background.blit(outline, (171,100)) 
     #background.blit(outline, (172,101))
     #background.blit(outline, (173,102))
     #background.blit(outline, (163,100)) # 1 up, 1 left
     #background.blit(outline, (164,100))
-    background.blit(outline, (165,100))
-    background.blit(outline, (166,100))
-    background.blit(outline, (167,100))
-    background.blit(outline, (168,101)) # 1 down, 1 left
-    background.blit(outline, (169,102))
-    background.blit(outline, (170,103))
-    background.blit(outline, (171,104))    
+    background.blit(outline, (text_horizontal - 2,text_vertical))
+    background.blit(outline, (text_horizontal - 1,text_vertical))
+    background.blit(outline, (text_horizontal,text_vertical))
+    background.blit(outline, (text_horizontal + 1,text_vertical + 1)) # 1 down, 1 left
+    background.blit(outline, (text_horizontal+2,text_vertical+2))
+    background.blit(outline, (text_horizontal+3,text_vertical+3))
+    background.blit(outline, (text_horizontal+4,text_vertical+4))    
     #background.blit(outline, (169,102))
     #background.blit(outline, (174,103))
     #background.blit(outline, (175,104))
     #background.blit(outline, (176,105))
     #background.blit(outline, (173,108))
     #background.blit(outline, (174,109))
-    background.blit(text, (167,100))
+    background.blit(text, (text_horizontal,text_vertical))
     #textpos = text.get_rect()
     #textpos.centerx = background.get_rect().centerx
     #textpos.centery = background.get_rect().centery
