31413d96c2aad1df528b56dc725c95dc4827598e
Grace Milton <gmmilton10@gmail.com>
2017-04-11 16:37:10 -0400

    Added start word

--- initialscreen.py
+++ initialscreen.py
@@ -15,33 +15,16 @@ def main():
 
     # Display title screen
     text_vertical = 100;
-    text_horizontal = 97; 
+    text_horizontal = 85; 
     outline_font = pygame.font.Font(None, 52)
-#<<<<<<< HEAD
-#    outline = outline_font.render("RecommeNDflix", 1, (10, 10, 10))
-#    title_font = pygame.font.Font(None, 52)
-#    title = title_font.render("RecommeNDflix", 1, (255, 255, 255))
-#    background.blit(outline, (165,99)) # 1 up, 1 left
-#    background.blit(outline, (166,99))
-#    background.blit(outline, (167,99))
-#    background.blit(outline, (168,99))
-#    background.blit(outline, (169,99))
-#=======
     outline = outline_font.render("R E C O M M E N D F L I X", 1, (10, 10, 10))
-    font = pygame.font.Font(None, 52)
-    text = font.render("R E C O M M E N D F L I X", 1, (255, 255, 255))
+    title_font = pygame.font.Font(None, 52)
+    title = title_font.render("R E C O M M E N D F L I X", 1, (255, 255, 255))
     background.blit(outline, (text_horizontal - 2,text_vertical - 1)) # 1 up, 1 left
     background.blit(outline, (text_horizontal - 1,text_vertical - 1))
     background.blit(outline, (text_horizontal,text_vertical - 1))
     background.blit(outline, (text_horizontal + 1,text_vertical - 1))
     background.blit(outline, (text_horizontal + 2,text_vertical - 1))
-#>>>>>>> e39f46c106ddc2a476cbec6bfd59a901470d937c
-    #background.blit(outline, (170,99)) # 1 down, 1 left
-    #background.blit(outline, (171,100)) 
-    #background.blit(outline, (172,101))
-    #background.blit(outline, (173,102))
-    #background.blit(outline, (163,100)) # 1 up, 1 left
-    #background.blit(outline, (164,100))
     background.blit(outline, (text_horizontal - 2,text_vertical))
     background.blit(outline, (text_horizontal - 1,text_vertical))
     background.blit(outline, (text_horizontal,text_vertical))
@@ -49,23 +32,10 @@ def main():
     background.blit(outline, (text_horizontal+2,text_vertical+2))
     background.blit(outline, (text_horizontal+3,text_vertical+3))
     background.blit(outline, (text_horizontal+4,text_vertical+4))    
-    #background.blit(outline, (169,102))
-    #background.blit(outline, (174,103))
-    #background.blit(outline, (175,104))
-    #background.blit(outline, (176,105))
-    #background.blit(outline, (173,108))
-    #background.blit(outline, (174,109))
-#<<<<<<< HEAD
-#    background.blit(title, (167,100))
-#    start_font = pygame.font.Font(None, 52)
-#    start = start_font.render("RecommeNDflix", 1, (255, 255, 255))
-#=======
-    background.blit(text, (text_horizontal,text_vertical))
-#>>>>>>> e39f46c106ddc2a476cbec6bfd59a901470d937c
-    #textpos = text.get_rect()
-    #textpos.centerx = background.get_rect().centerx
-    #textpos.centery = background.get_rect().centery
-    #background.blit(text, textpos)
+    background.blit(title, (text_horizontal,text_vertical))
+    start_font = pygame.font.Font(None, 40)
+    start = start_font.render("Start", 1, (200, 200, 200))
+    background.blit(start, (text_horizontal + 185, text_vertical + 150))
 
     # Blit everything to the screen
     screen.blit(background, (0, 0))
