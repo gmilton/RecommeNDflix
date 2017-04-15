1b63b3eb676871077916932fe49cad7317b840e1
Kathryn Sarah Lutz <klutz1@nd.edu>
2017-04-15 14:05:21 -0400

    Displays movie list by genre and amount of time to watch something

--- ap15.py
+++ ap15.py
@@ -7,11 +7,13 @@ import json
 import re
 GENRE = 35
 
-LIMIT = 5
+MOVIELIMIT = 5
+TIMELIMIT = 200
 def usage(status=0):
     print '''Usage: {} . [ -g GENRE -n NUMITEMS ]
         -g      GENRE       Genre of the show you want (default: Comedy)
-	-n 	LIMIT	    Limit the number of items you want
+	-n 	MOVIELIMIT  Limit the number of items you want
+        -t	TIMELIMIT   Limt the amount of time you want to watch
     '''.format(os.path.basename(sys.argv[0]))
     sys.exit(status)
 
@@ -24,7 +26,9 @@ while len(args) and args[0].startswith('-') and len(args[0]) > 1:
     elif arg == '-g':
 	GENRE = args.pop(0)	
     elif arg == '-n':
-        LIMIT = int(args.pop(0))
+        MOVIELIMIT = int(args.pop(0))
+    elif arg == '-t':
+        TIMELIMIT = int(args.pop(0))
     else: 
 	usage(1)
 
@@ -32,16 +36,26 @@ while len(args) and args[0].startswith('-') and len(args[0]) > 1:
 url1 = "https://api.themoviedb.org/3/genre/"
 url2 = "/movies?api_key=2fa8f55ea7e61c1512068ada4ad5b25a&language=en-US"
 URL = url1+str(GENRE)+url2
+omdburl = 'http://www.omdbapi.com/?'
+tmdbresponse = requests.get(URL)
 
-response = requests.get(URL)
+tmdbdata = tmdbresponse.json()
 
-alldata = response.json()
-
+#print omdbdata.keys()
 i = 1
-for title in alldata['results']:
-    if (i <= LIMIT): 
-        print title['title'], title['id']
-       # print title['id']
-        i = i + 1
-
+for movie in tmdbdata['results']:
+    if (i <= MOVIELIMIT):
+       # for title in omdbdata
+        tit = movie['title']
+	omdbresponse=requests.get(omdburl+'t='+tit)
+        omdbdata=omdbresponse.json()
+        
+        st = omdbdata['Runtime']
+        #print st
+        st = st[:-4]
+        stnum = int(st)
+        if (stnum <= TIMELIMIT):
+            print tit
+            print stnum
+            i = i + 1
 # vim: set sts=4 sw=4 ts=8 expandtab ft=cpp
